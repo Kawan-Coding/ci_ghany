c488fb9aa71bb450c3bc4301510e2bfdebf9343d
rudiwawa <rudiwawa1980@gmail.com>
2019-12-15 17:50:10 +0700

    perbaikan sintaks

--- application/views/content/ap_profil_admin.php
+++ application/views/content/ap_profil_admin.php
@@ -3,20 +3,20 @@
     <div class="dropdown-divider"></div>
 
     <div class="row">
-        <div class="col-lg-4" style="padding: 12px;">
-            <div class="border rounded d-lg-flex shadow-lg bgprofil" style="background-color: #4f9002;width: 100%;height: 223px;">
-                <img class="justify-content-center m-auto pp" style="width: 148px;max-height: auto;height: 148px;" src="<?= base_url() ?>assets/img/aaa.jpg">
+        <div class="col-lg-4 p-2" >
+            <div class="border rounded d-lg-flex shadow-sm  bgprofil h-100" >
+                <img class="justify-content-center m-auto pp img-cover"  src="<?= base_url() ?>assets/img/aaa.jpg">
             </div>
         </div>
-        <div class="col" style="padding: 12px;">
-            <div class="border rounded  shadow-lg" style="background-color: #ffffff;width: 100%;height: 223px;padding:24px">
+        <div class="col p-2">
+            <div class="border rounded  shadow-sm p-4">
                 <div class="row higlight">
                     <div class="col-lg-6">
-                        <div style=""><label class="d-block">Nama</label><strong class="d-block">Luna Maya</strong></div>
-                        <div style="margin-top: 16px;"><label class="d-block">Posisi</label><strong class="d-block">Admin Pusat</strong></div>
+                        <div ><label class="d-block">Nama</label><strong class="d-block">Luna Maya</strong></div>
+                        <div class="pt-4"><label class="d-block">Posisi</label><strong class="d-block">Admin Pusat</strong></div>
                     </div>
                     <div class="col-lg-6">
-                        <div style="margin-top: 0px;"><label class="d-block">status</label><strong class="d-block">Aktif</strong></div>
+                        <div ><label class="d-block">status</label><strong class="d-block">Aktif</strong></div>
                     </div>
                 </div>
 
@@ -25,18 +25,18 @@
     </div>
     <div>
 
-        <div style="margin-top: 24px;">
+        <div class="pt-4" >
             <label class="d-block">Username</label>
             <strong class="d-block">Ariel_lovely</strong>
         </div>
-        <div style="margin-top: 24px;">
+        <div class="pt-4">
             <label class="d-block">Password</label>
             <strong class="d-block">NTAP</strong></div>
-        <div style="margin-top: 24px;">
+        <div class="pt-4">
             <label class="d-block">Email</label>
             <strong class="d-block">Ariel_lovely@mail.com</strong>
         </div>
-        <div style="margin-top: 24px;">
+        <div class="pt-4">
             <label class="d-block">Nomor Telepon</label>
             <strong class="d-block">0857-9899-8888</strong>
         </div>
@@ -57,8 +57,8 @@
         color: #DD5251;
     }
 
-    .bgprofil {
+    /* .bgprofil {
         background: rgb(255, 255, 255);
         background: linear-gradient(0deg, rgba(255, 255, 255, 1) 50%, rgba(79, 144, 2, 1) 50%);
-    }
+    } */
 </style>
